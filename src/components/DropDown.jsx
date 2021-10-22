06c1d30496bd7aa60d0fc22fd27a1c58c97d52bc
Mark Stevens <markspstevens@yahoo.co.uk>
2021-10-22 11:48:22 +0200

    dropdown almost complete

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,39 +1,33 @@
 import { Component } from "react"
-import '../styles/DropDown.css'
-import about from '../data/kasaValues.json'
+import upArrow from '../assets/arrows/up_arrow_white.svg'
+import downArrow from '../assets/arrows/down_arrow_white.svg'
+import '../styles/Dropdown.css'
 
 export default class DropDown extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-        open : false,
-    }
-   }
-   
-   handleButtonClick = () => {
-    this.setState(state => ({        
-        open: !state.open     
-    }))
-  }
+    state = { open: false }
 
-    render() { 
+    handleButtonClick = () => {
+        this.setState(state => ({        
+            open: !state.open     
+        }))
+      }
+    
+  render() {
     const { open } = this.state
-
-      return (
-        <article>
-          {about.map((data) => (
-          <div key= {data.id} className="accordion">
-              
-                <div className="accordion_title" onClick={() => this.handleButtonClick}>
-                  <div>{data.title}</div>
-                  <div>{open ? '-' : '+'}</div>
-                </div>
-                <div className="accordion_content">{data.content}</div>
-           
-          </div> ))}
-        </article>
-      )
-    }
+    const {id, title, content} = this.props
+    return (
+        
+            <div key={id} className="dropDown" onClick={this.handleButtonClick}>
+                {title}
+                     <div>{open ? <img src={upArrow} alt="icon" /> : <img src={downArrow} alt="icon" />}</div>
+                {open && (
+                    <div className="dropdownList">
+                        <ul>
+                            <li>{content}</li>
+                        </ul>
+                    </div>
+                )}
+            </div>   
+    )
   }
-
-  
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,24 @@
 import { Component } from "react"
 import Banner from '../components/Banner'
 import aboutBanner from '../assets/aboutBanner.jpg'
+import aboutData from '../data/aboutDropdownData.json'
+import DropDown from '../components/Dropdown'
 import '../styles/About.css'
-import DropDown from "../components/Drop"
-  
+
 export default class About extends Component {
  
   render() { 
     return ( 
-        <main className="aboutWrapper">
-        <div className='aboutBanner'>
-            <Banner image={aboutBanner} alternate='A Mountain Scene'/>
-            {/* <h1 className='slogan'> {values}</h1> */}
-        </div>
-        <DropDown />
+      <main>
+        <div className="aboutWrapper">
+          <div className='aboutBanner'>
+              <Banner image={aboutBanner} alternate='A Mountain Scene'/>
+              {/* <h1 className='slogan'> {values}</h1> */}
+          </div>     
+            {aboutData.map((data) => (
+            <DropDown id={data.id} title={data.title} content={data.content}/>
+            ))}   
+      </div>
       </main>
     )
   }
