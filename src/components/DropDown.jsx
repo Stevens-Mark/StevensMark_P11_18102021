3606c62d688e81391c29d1f07f5dca21305233de
Mark Stevens <markspstevens@yahoo.co.uk>
2021-10-24 13:29:54 +0200

    slight change to height

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,22 +1,47 @@
-import { Component } from "react"
+import { Component, createRef } from "react"
 import upArrow from '../assets/arrows/up_arrow_white.svg'
 import downArrow from '../assets/arrows/down_arrow_white.svg'
 import '../styles/Dropdown.css'
 
 export default class DropDown extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.container = createRef();
+    //   }
+      
     state = { open: false }
-
+  
     handleButtonClick = () => {
         this.setState(state => ({        
             open: !state.open     
         }))
       }
-    
+
+    //   handleClickOutside = (event) => {
+    //     if (
+    //       this.container.current &&
+    //       !this.container.current.contains(event.target)
+    //     ) {
+    //       this.setState({
+    //         open: false,
+    //       });
+    //     }
+    //   };
+
+    //   componentDidMount() {
+    //     document.addEventListener("mousedown", this.handleClickOutside);
+    // }
+
+    // componentWillUnmount() {
+    //   document.removeEventListener("mousedown", this.handleClickOutside);
+    // }
+
+  
   render() {
     const { open } = this.state
     const {id, title, content} = this.props
     return (
-        <div className='dropDown'>
+        <div className='dropDown' /*ref={this.container}*/>
             <div className="dropDownTitle" onClick={this.handleButtonClick}>
                 <span>{title}</span>
                 <span>{open ? (<img src={upArrow} alt="icon" />) : 
