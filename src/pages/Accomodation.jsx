e86a1c53ae63dd0b1a6d012fdc92ccb25458aa95
Mark Stevens <markspstevens@yahoo.co.uk>
2021-10-25 11:00:27 +0200

    base for fetch start

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -6,13 +6,39 @@ import Tags from '../components/Tags'
 
 import '../styles/Accomodation.css'
  
-export default class Accomodation extends Component {
-    render() {
-        const accomodationId = this.props.match.params.id
-        const chosenAccomodation = accomodationData.find((data) => data.id === accomodationId)
 
-        const {id, pictures, title, location, tags, host, rating, description, equipments,
-        } = chosenAccomodation
+    export default class Accomodation extends Component {
+        constructor(props) {
+            super(props)
+            this.state = {
+                profileData: {},
+            }
+        }
+        
+        componentDidMount() {
+            const { id } = this.props.match.params
+    
+        //     fetch(`http://localhost:8000/freelance?id=${id}`)
+        //     .then((response) => response.json())
+        //     .then((jsonResponse) => {
+        //         this.setState({ profileData: jsonResponse?.freelanceData })
+        //     })
+        // }
+    
+            const chosenAccomodation = accomodationData.find((data) => data.id === id)
+            this.setState({ profileData: chosenAccomodation })
+        }
+    
+    render() {
+        const { profileData } = this.state
+        const {
+            id, pictures, title, location, tags, host, rating, description, equipments,
+        } = profileData
+        // const accomodationId = this.props.match.params.id
+        // const chosenAccomodation = accomodationData.find((data) => data.id === accomodationId)
+        // const {id, pictures, title, location, tags, host, rating, description, equipments,
+        // } = chosenAccomodation
+
         
         return (
             <main>
@@ -23,9 +49,9 @@ export default class Accomodation extends Component {
                             <h1 className='accomodationTitle'>{title} </h1>
                             <p className='accomodationLocation'>{location}</p>    
                         </div>
-                        <Host name={host.name} picture={host.picture}/>
+                        {/* <Host name={host.name} picture={host.picture}/> */}
                     </div>
-                    <Tags tagData={tags}/>
+                    {/* <Tags tagData={tags}/> */}
                 </div>
                 
             </main>
