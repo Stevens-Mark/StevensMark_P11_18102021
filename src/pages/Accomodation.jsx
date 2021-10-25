7231949c842f928d5eeddef76d5ff3d552c7c523
Mark Stevens <markspstevens@yahoo.co.uk>
2021-10-25 11:57:54 +0200

    none

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -7,37 +7,37 @@ import Tags from '../components/Tags'
 import '../styles/Accomodation.css'
  
 
-    export default class Accomodation extends Component {
-        constructor(props) {
-            super(props)
-            this.state = {
-                profileData: {},
-            }
+export default class Accomodation extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            profileData: {},
         }
+    }
         
-        componentDidMount() {
-            const { id } = this.props.match.params
-    
-        //     fetch(`http://localhost:8000/freelance?id=${id}`)
-        //     .then((response) => response.json())
-        //     .then((jsonResponse) => {
-        //         this.setState({ profileData: jsonResponse?.freelanceData })
-        //     })
-        // }
-    
-            const chosenAccomodation = accomodationData.find((data) => data.id === id)
-            this.setState({ profileData: chosenAccomodation })
-        }
-    
+    componentDidMount() {
+        const { id } = this.props.match.params
+
+    //     fetch(`http://localhost:8000/freelance?id=${id}`)
+    //     .then((response) => response.json())
+    //     .then((jsonResponse) => {
+    //         this.setState({ profileData: jsonResponse?.freelanceData })
+    //     })
+    // }
+
+        const chosenAccomodation = accomodationData.find((data) => data.id === id)
+        this.setState({ profileData: chosenAccomodation })
+    }
+
     render() {
-        const { profileData } = this.state
-        const {
-            id, pictures, title, location, tags, host, rating, description, equipments,
-        } = profileData
-        // const accomodationId = this.props.match.params.id
-        // const chosenAccomodation = accomodationData.find((data) => data.id === accomodationId)
-        // const {id, pictures, title, location, tags, host, rating, description, equipments,
-        // } = chosenAccomodation
+        // const { profileData } = this.state
+        // const {
+        //     id, pictures, title, location, tags, host, rating, description, equipments,
+        // } = profileData
+        const accomodationId = this.props.match.params.id
+        const chosenAccomodation = accomodationData.find((data) => data.id === accomodationId)
+        const {id, pictures, title, location, tags, host, rating, description, equipments,
+        } = chosenAccomodation
 
         
         return (
@@ -48,8 +48,9 @@ import '../styles/Accomodation.css'
                         <div>
                             <h1 className='accomodationTitle'>{title} </h1>
                             <p className='accomodationLocation'>{location}</p>    
+
                         </div>
-                        {/* <Host name={host.name} picture={host.picture}/> */}
+                        <Host name={host.name} picture={host.picture}/>
                     </div>
                     {/* <Tags tagData={tags}/> */}
                 </div>
