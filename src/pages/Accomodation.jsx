33255299671d2da70f057da5a3cf06a6036090e4
Mark Stevens <markspstevens@yahoo.co.uk>
2021-10-27 11:23:33 +0200

    base for experiment

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -5,81 +5,82 @@ import Host from '../components/Host'
 import Tags from '../components/Tags'
 import DropDown from '../components/DropDown'
 import Ratings from '../components/Ratings'
-import Error from './Error'
+// import Error from './Error'
 
 import '../styles/Accomodation.css'
  
 
 export default class Accomodation extends Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         profileData: {},
-    //     }
-    // }
+    constructor(props) {
+        super(props)
+        this.state = {
+            profileData: {},
+        }
+    }
         
-    // componentDidMount() {
-    //     const { id } = this.props.match.params
+    componentDidMount() {
+        const { id } = this.props.match.params
 
-    // //     fetch('../data/logements.json')
-    // //     .then((response) => response.json()
-    // //     .then(({ surveyData }) => console.log(surveyData))
-    // //     .catch((error) => console.log(error))
-    // // )
-    // //     fetch(`http://localhost:8000/freelance?id=${id}`)
-    // //     .then((response) => response.json())
-    // //     .then((jsonResponse) => {
-    // //         this.setState({ profileData: jsonResponse?.freelanceData })
-    // //     })
-    // // }
-
-    //     const chosenAccomodation = accomodationData.find((data) => data.id === id)
-    //     this.setState({ profileData: chosenAccomodation })
+    //     fetch('../data/logements.json')
+    //     .then((response) => response.json()
+    //     .then(({ surveyData }) => console.log(surveyData))
+    //     .catch((error) => console.log(error))
+    // )
+    //     fetch(`http://localhost:8000/freelance?id=${id}`)
+    //     .then((response) => response.json())
+    //     .then((jsonResponse) => {
+    //         this.setState({ profileData: jsonResponse?.freelanceData })
+    //     })
     // }
+
+        const chosenAccomodation = accomodationData.find((data) => data.id === id)
+        this.setState({ profileData: chosenAccomodation })
+    }
 
     render() {
-        // const { profileData } = this.state
-        // const {
-        //     id, pictures, title, location, tags, host, rating, description, equipments,
-        // } = profileData
+        const { profileData } = this.state
+        const {
+            id, pictures, title, location, tags, host, rating, description, equipments,
+        } = profileData
 
-        const accomodationId = this.props.match.params.id
-        const chosenAccomodation = accomodationData.find((data) => data.id === accomodationId)
+        // const accomodationId = this.props.match.params.id
+        // const chosenAccomodation = accomodationData.find((data) => data.id === accomodationId)
 
-        if (chosenAccomodation) {
-            const {pictures, title, location, tags, host, rating, description, equipments,
-            } = chosenAccomodation
+        // if (chosenAccomodation) {
+        //     const {pictures, title, location, tags, host, rating, description, equipments,
+        //     } = chosenAccomodation
             
             return (
                 <main>
                     <div className='accomodationWrapper'>
-                        <Carousel photoAlbum={pictures}/>
+                        {/* <Carousel photoAlbum={pictures}/> */}
                         <div className='accomodationHeader'>
 
                             <div>
                                 <h1 className='accomodationTitle'>{title} </h1>
                                 <p className='accomodationLocation'>{location}</p> 
-                                <Tags tagData={tags} />
+                                {/* <Tags tagData={tags} /> */}
                             </div>
 
                             <div className='hostSummary'>
-                                <Host name={host.name} picture={host.picture}/>
-                                <Ratings ratingNumber={rating}/>
+                                {/* <Host name={host.name} picture={host.picture}/>
+                                <Ratings ratingNumber={rating}/> */}
                             </div>
                         </div>   
 
                         <div className='accomodationDetails'>
-                            <DropDown dropdownPurpose='DropdownAccomPage' dropdownType='dropDownListAccomodation' title={'Description'} content={description}/>
-                            <DropDown dropdownPurpose='DropdownAccomPage' dropdownType='dropDownListAccomodation'  title={'Equipment'} content={equipments}/>
+                            {/* <DropDown dropdownPurpose='DropdownAccomPage' dropdownType='dropDownListAccomodation' title={'Description'} content={description}/>
+                            <DropDown dropdownPurpose='DropdownAccomPage' dropdownType='dropDownListAccomodation'  title={'Equipment'} content={equipments}/> */}
                         </div>
                     </div>       
                 </main>
             )
-            } else {
-                return (
-                <Error />
-                )
-            }
-    }
+            } 
+    //         else {
+    //             return (
+    //             <Error />
+    //             )
+    //         }
+    // }
 }
  
