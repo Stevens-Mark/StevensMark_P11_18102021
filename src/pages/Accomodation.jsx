401d9978589956de653a77245f47980dadbda597
Mark Stevens <markspstevens@yahoo.co.uk>
2021-10-27 12:00:49 +0200

    base

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -11,30 +11,21 @@ import '../styles/Accomodation.css'
  
 
 export default class Accomodation extends Component {
+
     constructor(props) {
         super(props)
         this.state = {
             profileData: {},
         }
     }
-        
+ 
     componentDidMount() {
         const { id } = this.props.match.params
-
-    //     fetch('../data/logements.json')
-    //     .then((response) => response.json()
-    //     .then(({ surveyData }) => console.log(surveyData))
-    //     .catch((error) => console.log(error))
-    // )
-    //     fetch(`http://localhost:8000/freelance?id=${id}`)
-    //     .then((response) => response.json())
-    //     .then((jsonResponse) => {
-    //         this.setState({ profileData: jsonResponse?.freelanceData })
-    //     })
-    // }
-
-        const chosenAccomodation = accomodationData.find((data) => data.id === id)
-        this.setState({ profileData: chosenAccomodation })
+ 
+        fetch(`../src/data/logements.json`)
+        .then((response) => response.json())
+        .then(({ surveyData }) => console.log(surveyData))
+        .catch((error) => console.log(error))
     }
 
     render() {
@@ -42,6 +33,27 @@ export default class Accomodation extends Component {
         const {
             id, pictures, title, location, tags, host, rating, description, equipments,
         } = profileData
+
+
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         profileData: {},
+    //     }
+    // }
+        
+    // componentDidMount() {
+    //     const { id } = this.props.match.params
+
+    //     const chosenAccomodation = accomodationData.find((data) => data.id === id)
+    //     this.setState({ profileData: chosenAccomodation })
+    // }
+
+    // render() {
+    //     const { profileData } = this.state
+    //     const {
+    //         id, pictures, title, location, tags, host, rating, description, equipments,
+    //     } = profileData
 
         // const accomodationId = this.props.match.params.id
         // const chosenAccomodation = accomodationData.find((data) => data.id === accomodationId)
