0ff836121349ee3027ff7f195a53fecc3b71be1b
Mark Stevens <markspstevens@yahoo.co.uk>
2021-11-01 09:58:15 +0100

    replace app.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './index.css'
-// import App from './App';
-import reportWebVitals from './reportWebVitals'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import About from './pages/About'
-import Accomodation from './pages/Accomodation'
-import Error from './pages/Error'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Header />   
-        <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route
-              path="/accomodation/:id"
-              render={(props) => <Accomodation {...props} />}
-            />
-            <Route>
-              <Error />
-            </Route>
-        </Switch> 
-      <Footer />
-    </Router>       
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
